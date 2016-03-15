befe73fc893d592440cbe0d28dfa5e5fa1094afb
David Hovemeyer <david.hovemeyer@gmail.com>
2016-03-15 15:23:55 -0400

    a couple C tests

---
+++ t/bananana.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    // Declare a variable to store the input value
+    // TODO
+    int n;
+    
+    // Read the input value
+    // TODO
+    scanf("%i", &n);
+    
+    // Print the output
+    // TODO
+    printf("ba");
+    for (int i = 0; i < n; i++) {
+        printf("na");
+    }
+    printf("\n");
+    
+    return 0;
+}
